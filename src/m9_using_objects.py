b7103344f95fcf20ca0ce78418ab1cee0597dd1a
harperis <harperis@rose-hulman.edu>
2018-09-12 12:16:53 -0400

    Done

--- src/m9_using_objects.py
+++ src/m9_using_objects.py
@@ -5,18 +5,35 @@ This module lets you practice  ** using objects **, including:
   -- accessing their DATA via INSTANCE VARIABLES
 
 Authors: David Mutchler, Dave Fisher, Vibha Alangar, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Isaac Harper.
+"""  # Done: 1. PUT YOUR NAME IN THE ABOVE LINE.
+
 
 import rosegraphics as rg
+import math
 
 
 def main():
+    two_circles()
+    circle_and_rectangle()
+    lines()
     """ Calls the other functions to demonstrate and/or test them. """
     # Test your functions by putting calls to them here:
 
 
 def two_circles():
+    window = rg.RoseWindow()
+    center_point1 = rg.Point(100, 100)
+    radius1 = 15
+    center_point2 = rg.Point(200, 172)
+    radius2 = 25
+    circle1 = rg.Circle(center_point1, radius1)
+    circle2 = rg.Circle(center_point2, radius2)
+    circle2.fill_color = 'red'
+    circle1.attach_to(window)
+    circle2.attach_to(window)
+    window.render()
+    window.close_on_mouse_click()
     """
     -- Constructs an rg.RoseWindow.
     -- Constructs and draws two rg.Circle objects on the window
@@ -27,7 +44,7 @@ def two_circles():
     -- Waits for the user to press the mouse, then closes the window.
     """
     # ------------------------------------------------------------------
-    # TODO: 2. Implement this function, per its green doc-string above.
+    # Done: 2. Implement this function, per its green doc-string above.
     #    -- ANY two rg.Circle objects that meet the criteria are fine.
     #    -- File  COLORS.pdf  lists all legal color-names.
     # Put a statement in   main   to test this function
@@ -36,6 +53,38 @@ def two_circles():
 
 
 def circle_and_rectangle():
+    window = rg.RoseWindow()
+    x = 251
+    y = 213
+    center_point = rg.Point(x, y)
+    radius = 35
+    circle = rg.Circle(center_point, radius)
+    circle.fill_color = 'blue'
+    circle.attach_to(window)
+    a = 50
+    b = 87
+    c = 100
+    d = 300
+    point1 = rg.Point(a, b)
+    point2 = rg.Point(c, d)
+    rectangle = rg.Rectangle(point1, point2)
+    center = rectangle.get_center()
+    rectangle.attach_to(window)
+    window.render()
+    print(circle.outline_thickness)
+    print(circle.fill_color)
+    print(circle.center)
+    print(x)
+    print(y)
+    print(rectangle.outline_thickness)
+    print(rectangle.fill_color)
+    print(center)
+    print((a + c) / 2)
+    print((b + d) / 2)
+    window.close_on_mouse_click()
+
+
+
     """
     -- Constructs an rg.RoseWindow.
     -- Constructs and draws a rg.Circle and rg.Rectangle
@@ -67,7 +116,7 @@ def circle_and_rectangle():
            150.0
     """
     # ------------------------------------------------------------------
-    # TODO: 3. Implement this function, per its green doc-string above.
+    # Done: 3. Implement this function, per its green doc-string above.
     #   -- ANY objects that meet the criteria are fine.
     # Put a statement in   main   to test this function
     #    (by calling this function).
@@ -78,6 +127,26 @@ def circle_and_rectangle():
 
 
 def lines():
+    window = rg.RoseWindow()
+    a = 10
+    b = 20
+    c = 300
+    d = 250
+    point1 = rg.Point(a, b)
+    point2 = rg.Point(c, d)
+    point3 = rg.Point(250, 123)
+    point4 = rg.Point(100, 75)
+    line1 = rg.Line(point1, point2)
+    line1.thickness = 15
+    line2 = rg.Line(point3, point4)
+    line1.attach_to(window)
+    line2.attach_to(window)
+    midpoint = rg.Line.get_midpoint(line1)
+    print(midpoint)
+    print((a + c) / 2)
+    print((b + d) / 2)
+    window.render()
+    window.close_on_mouse_click()
     """
     -- Constructs a rg.RoseWindow.
     -- Constructs and draws on the window two rg.Lines such that:
@@ -99,7 +168,7 @@ def lines():
 
     -- Waits for the user to press the mouse, then closes the window.
     """
-    # TODO: 4. Implement and test this function.
+    # Done: 4. Implement and test this function.
 
 
 # ----------------------------------------------------------------------
--- src/m9b_summing_again.py
+++ src/m9b_summing_again.py
@@ -4,8 +4,8 @@ in its simplest classic forms:
    SUMMING:       total = total + number
 
 Authors: David Mutchler, Dave Fisher, Vibha Alangar, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Isaac Harper.
+"""  # Done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
 def main():
@@ -17,7 +17,7 @@ def main():
 def run_test_sum_powers():
     """ Tests the   sum_powers   function. """
     # ------------------------------------------------------------------
-    # TODO: 2. Implement this function.
+    # Done: 2. Implement this function.
     #   It TESTS the  sum_powers  function defined below.
     #   Include at least **   3   ** tests.
     #
@@ -28,9 +28,31 @@ def run_test_sum_powers():
     print('--------------------------------------------------')
     print('Testing the   sum_powers   function:')
     print('--------------------------------------------------')
+    expected = 3.80826
+    actual = sum_powers(5, -0.3)
+    print('expected', expected)
+    print('actual', actual)
+    expected = '????'
+    answer = sum_powers(4, .8)
+    print('expected', expected)
+    print('answer', answer)
+    expected = "?????"
+    answer = sum_powers(10, -0.5)
+    print('expected', expected)
+    print('answer', answer)
+    expected = '????'
+    answer = sum_powers(8, 1.3)
+    print('expected', expected)
+    print('answer', answer)
+
 
 
 def sum_powers(n, p):
+    total = 0
+    power = p
+    for k in range(n):
+        total = total + ((k + 1) ** power)
+    return total
     """
     What comes in:  A non-negative integer n
                     and a number p.
@@ -43,7 +65,7 @@ def sum_powers(n, p):
       -- sum_powers(100, 0.1) returns about 144.45655
     """
     # ------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # Done: 3. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     #
     #   No fair running the code of  sum_powers  to GENERATE
@@ -54,7 +76,7 @@ def sum_powers(n, p):
 def run_test_sum_powers_in_range():
     """ Tests the   sum_powers_in_range   function. """
     # ------------------------------------------------------------------
-    # TODO: 4. Implement this function.
+    # Done: 4. Implement this function.
     #   It TESTS the  sum_powers_in_range  function defined below.
     #   Include at least **   3   ** tests.
     #
@@ -65,9 +87,32 @@ def run_test_sum_powers_in_range():
     print('--------------------------------------------------')
     print('Testing the   sum_powers_in_range   function:')
     print('--------------------------------------------------')
+    expected = 142.384776
+    answer = sum_powers_in_range(3, 100, 0.1)
+    print('Test 1 expected', expected)
+    print('Test 1 answer', answer)
+    expected = '????'
+    answer = sum_powers_in_range(5, 25, 0.7)
+    print('expected', expected)
+    print('answer', answer)
+    expected = '?????'
+    answer = sum_powers_in_range(7, 15, -0.4)
+    print('expected', expected)
+    print('answer', answer)
+    expected = '????'
+    answer = sum_powers_in_range(27, 63, 0.2)
+    print('expected', expected)
+    print('answer', answer)
 
 
 def sum_powers_in_range(m, n, p):
+    total = 0
+    power = p
+    for k in range(m, n + 1):
+        total = total + ((k) ** power)
+    return total
+
+
     """
     What comes in:  Non-negative integers m and n, with n >= m,
                     and a number p.
@@ -78,17 +123,16 @@ def sum_powers_in_range(m, n, p):
     Side effects:  None.
     Example:
       -- sum_powers_in_range(3, 100, 0.1) returns about 142.384776
-    """
     # ------------------------------------------------------------------
-    # TODO: 5. Implement and test this function.
+    # Done: 5. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     #
     #   No fair running the code of  sum_powers_in_range  to GENERATE
     #   test cases; that would defeat the purpose of TESTING!
     # ------------------------------------------------------------------
 
-
 # ----------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
 # ----------------------------------------------------------------------
+    """
 main()
